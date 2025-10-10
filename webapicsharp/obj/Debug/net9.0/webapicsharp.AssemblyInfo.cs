82605b1b79dd1d035c2bf0f9e72d400b5ddec97d
Jeferson <jefersondavidvegacardona@gmail.com>
2025-10-10 09:23:47 -0500

    my first push

--- webapicsharp/obj/Debug/net9.0/webapicsharp.AssemblyInfo.cs
+++ webapicsharp/obj/Debug/net9.0/webapicsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapicsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ceb1e14c8116f689c2291b28817e7418130b638")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapicsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapicsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
